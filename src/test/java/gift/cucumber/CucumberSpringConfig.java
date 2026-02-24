b1017be80f3ee95a4d408f1b48c8fd4462b97278
ianlee2 <ianleeee0@gmail.com>
2026-02-24 16:40:19 +0900

    refactor(test): CucumberSpringConfig를 랜덤 포트 기반으로 변경해 test(H2)와 cucumberTest(PostgreSQL) 실행 분리

--- src/test/java/gift/cucumber/CucumberSpringConfig.java
+++ src/test/java/gift/cucumber/CucumberSpringConfig.java
@@ -4,16 +4,17 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfig {
+
+    @LocalServerPort
+    int port;
 
     @PostConstruct
     void setUp() {
-        RestAssured.baseURI = "http://localhost";
-        RestAssured.port = 28080;
+        RestAssured.port = port;
     }
 }
